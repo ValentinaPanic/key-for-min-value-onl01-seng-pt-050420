24b21ba249715223a25108d55f12b3aab8d88144
Valentina Panic <valentina.panic87@gmail.com>
2020-05-15 05:03:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-min_key,min_val = hash.first
+min_key,min_val = name_hash.first
 name_hash.each do |k,v|
   if v < min_val
     min_val = v
